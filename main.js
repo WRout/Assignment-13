264c9c1c2ffb9f1fd0f03627c50838f03a15fb90
WRout <55300028+WRout@users.noreply.github.com>
2020-04-23 15:13:13 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+
+
 function getInput() {
-    
+    let gender = prompt("What gender shoes are you looking for? (Mens or Womens)", "men's");
+    if ((gender.toUpperCase() === "MENS") || (gender.toUpperCase === "MEN'S") || (gender.toUpperCase === "MEN")){
+        let size = prompt("What foot size are you?");
+    } else if ((gender.toUpperCase() === "WOMENS") || (gender.toUpperCase === "WOMEN'S") || (gender.toUpperCase === "WOMEN")){
+        
+    } else {
+        alert("Invalid gender! Please try again!");
+        getInput();
+    }
 }
