fa220daffa2d349f0d90fd5eaaf71b5740f69595
WRout <55300028+WRout@users.noreply.github.com>
2020-04-23 15:22:08 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
 
 
 function getInput() {
-    let gender = prompt("What gender shoes are you looking for? (Mens or Womens)", "men's");
+    let getGender = prompt("What gender shoes are you looking for? (Mens or Womens)", "men's");
+    let getSize = prompt("What foot size are you?");
+    
+    return {
+        userGender : getGender,
+        footSize : getSize,
+        logInfo : function(){
+            console.log(this.userGender + " : " + this.footSize);
+        }
+    }
+}
+
+
+function processInfo() {
+    let info = getInput();
+    let gender = info.userGender;
+    let size = info.footSize;
+    info.logInfo();
+    
     if ((gender.toUpperCase() === "MENS") || (gender.toUpperCase === "MEN'S") || (gender.toUpperCase === "MEN")){
-        let size = prompt("What foot size are you?");
+        
     } else if ((gender.toUpperCase() === "WOMENS") || (gender.toUpperCase === "WOMEN'S") || (gender.toUpperCase === "WOMEN")){
         
     } else {
